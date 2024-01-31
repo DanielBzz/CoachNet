84c0fd94ed93a0c8dd74a559abb3e3dfe0afe99c
danielBzz <danielbaz199@gmail.com>
2024-01-31 17:36:17 +0200

    starter spring project

---
+++ coachNet/src/main/java/CoachNet/backend/coachNet/CoachNetApplication.java
@@ -0,0 +1,13 @@
+package CoachNet.backend.coachNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachNetApplication.class, args);
+	}
+	
+}
---
+++ coachNet/src/test/java/CoachNet/backend/coachNet/CoachNetApplicationTests.java
@@ -0,0 +1,13 @@
+package CoachNet.backend.coachNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
